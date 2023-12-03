7991d752bdf5540e8f37c2a977f18fc11982319a
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-12-03 08:44:21 -0400

    refactor: inject use cases in resolvers constructors

--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/graphql/CustomerResolver.java
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/graphql/CustomerResolver.java
@@ -3,7 +3,6 @@ package br.com.fullcycle.hexagonal.infrastructure.graphql;
 import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
 import br.com.fullcycle.hexagonal.application.usecases.GetCustomerByIdUseCase;
 import br.com.fullcycle.hexagonal.infrastructure.dtos.CustomerDTO;
-import br.com.fullcycle.hexagonal.infrastructure.services.CustomerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -12,14 +11,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class CustomerResolver {
 
-  private final CustomerService customerService;
+  private final CreateCustomerUseCase createCustomerUseCase;
 
-  public CustomerResolver(final CustomerService customerService) { this.customerService = customerService; }
+  private final GetCustomerByIdUseCase getCustomerByIdUseCase;
+
+  public CustomerResolver(
+    final CreateCustomerUseCase createCustomerUseCase,
+    final GetCustomerByIdUseCase getCustomerByIdUseCase
+  ) {
+    this.createCustomerUseCase = createCustomerUseCase;
+    this.getCustomerByIdUseCase = getCustomerByIdUseCase;
+  }
 
   @MutationMapping
   public CreateCustomerUseCase.Output createCustomer(@Argument final CustomerDTO input) {
-    final var createCustomerUseCase = new CreateCustomerUseCase(this.customerService);
-    final var output = createCustomerUseCase.execute(new CreateCustomerUseCase.Input(
+    final var output = this.createCustomerUseCase.execute(new CreateCustomerUseCase.Input(
       input.getCpf(),
       input.getEmail(),
       input.getName()
@@ -29,8 +35,7 @@ public class CustomerResolver {
 
   @QueryMapping
   public GetCustomerByIdUseCase.Output customerOfId(@Argument final Long id) {
-    final var getCustomerByIdUseCase = new GetCustomerByIdUseCase(this.customerService);
-    return getCustomerByIdUseCase.execute(new GetCustomerByIdUseCase.Input(id))
+    return this.getCustomerByIdUseCase.execute(new GetCustomerByIdUseCase.Input(id))
       .orElse(null);
   }
 
--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/graphql/PartnerResolver.java
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/graphql/PartnerResolver.java
@@ -3,7 +3,6 @@ package br.com.fullcycle.hexagonal.infrastructure.graphql;
 import br.com.fullcycle.hexagonal.application.usecases.CreatePartnerUseCase;
 import br.com.fullcycle.hexagonal.application.usecases.GetPartnerByIdUseCase;
 import br.com.fullcycle.hexagonal.infrastructure.dtos.PartnerDTO;
-import br.com.fullcycle.hexagonal.infrastructure.services.PartnerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -12,14 +11,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PartnerResolver {
 
-  private final PartnerService partnerService;
+  private final CreatePartnerUseCase createPartnerUseCase;
 
-  public PartnerResolver(final PartnerService partnerService) { this.partnerService = partnerService; }
+  private final GetPartnerByIdUseCase getPartnerByIdUseCase;
+
+  public PartnerResolver(
+    final CreatePartnerUseCase createPartnerUseCase,
+    final GetPartnerByIdUseCase getPartnerByIdUseCase
+  ) {
+    this.createPartnerUseCase = createPartnerUseCase;
+    this.getPartnerByIdUseCase = getPartnerByIdUseCase;
+  }
 
   @MutationMapping
   public CreatePartnerUseCase.Output createPartner(@Argument final PartnerDTO input) {
-    final var createPartnerUseCase = new CreatePartnerUseCase(this.partnerService);
-    return createPartnerUseCase.execute(new CreatePartnerUseCase.Input(
+    return this.createPartnerUseCase.execute(new CreatePartnerUseCase.Input(
       input.getCnpj(),
       input.getEmail(),
       input.getName()
@@ -28,8 +34,7 @@ public class PartnerResolver {
 
   @QueryMapping
   public GetPartnerByIdUseCase.Output partnerOfId(@Argument final Long id) {
-    final var getPartnerByIdUseCase = new GetPartnerByIdUseCase(this.partnerService);
-    return getPartnerByIdUseCase.execute(new GetPartnerByIdUseCase.Input(id))
+    return this.getPartnerByIdUseCase.execute(new GetPartnerByIdUseCase.Input(id))
       .orElse(null);
   }
 
